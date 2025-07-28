5a15dd99cf573be1aefdb1688cd76aa9bd157322
xiaoabiao <36405417+xiaoabiao@users.noreply.github.com>
2025-07-28 22:41:39 +0800

    new

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://202.168.170.175:21114".to_owned()
 }
 
 #[inline]
